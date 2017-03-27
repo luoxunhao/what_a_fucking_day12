23bbd3e6fec4b6362c01c00b3173a747be3f303d
luoxunhao <1215070400@qq.com>
2017-03-27 16:35:42 +0800

    最长回文子序列

--- src/algs/dp/LongestPalindromeSubsquence.java
+++ src/algs/dp/LongestPalindromeSubsquence.java
@@ -1,9 +1,10 @@
-package algs.string;
+package algs.dp;
 
 import java.util.Scanner;
 
 /**
- * Created by lxh on 2017/3/24.
+ * 给定一个字符串s，你可以从中删除一些字符，使得剩下的串是一个回文串。如何删除才能使得回文串最长呢？
+ 输出需要删除的字符个数。
  */
 public class LongestPalindromeSubsquence {
 
