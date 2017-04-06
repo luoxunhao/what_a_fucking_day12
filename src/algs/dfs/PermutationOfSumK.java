c9ec4d4c9d54b044e23f1dbbed61ab8fb63d025a
luoxunhao <1215070400@qq.com>
2017-04-06 12:56:08 +0800

    dfs

--- src/algs/dfs/PermutationOfSumK.java
+++ src/algs/dfs/PermutationOfSumK.java
@@ -11,6 +11,7 @@ public class PermutationOfSumK {
     private static int n;
     private static int m;
 
+    //打印组合
     public static void dfs(ArrayList<Integer> res, int pos, int n, int sum, int m){
         if (pos > n){
             return;
@@ -28,6 +29,38 @@ public class PermutationOfSumK {
         dfs(new ArrayList<>(res),pos+1,n,sum-pos,m);
     }
 
+    //是否存在
+    public static boolean dfs1(int[] a, int pos, int n, int sum, int m){
+        if (pos == n){
+            return sum == m;
+        }
+        sum += a[pos];
+        if (sum == m){
+            return true;
+        }else if (sum > m){
+            return false;
+        }
+        return dfs1(a,pos+1,n,sum,m) || dfs1(a,pos+1,n,sum-a[pos],m);
+    }
+
+    //组合数
+    public static int dfs2(int[] a, int pos, int n, int sum, int m){
+        if (pos == n){
+            if (sum == m){
+                return 1;
+            }else {
+                return 0;
+            }
+        }
+        sum += a[pos];
+        if (sum == m){
+            return 1;
+        }else if (sum > m){
+            return 0;
+        }
+        return dfs2(a,pos+1,n,sum,m) + dfs2(a,pos+1,n,sum-a[pos],m);
+    }
+
     public static void print(ArrayList<Integer> list){
         for (int i = 0; i < list.size(); i++){
             if (i == list.size()-1){
@@ -43,9 +76,14 @@ public class PermutationOfSumK {
         while (sc.hasNext()){
             n = sc.nextInt();
             m = sc.nextInt();
-            n = n > m ? m : n;
-            ArrayList<Integer> res = new ArrayList<>();
-            dfs(res,1,n,0,m);
+            //n = n > m ? m : n;
+            //ArrayList<Integer> res = new ArrayList<>();
+            //dfs(res,1,n,0,m);
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++){
+                a[i] = sc.nextInt();
+            }
+            System.out.println(dfs2(a,0,n,0,m));
         }
     }
 }
