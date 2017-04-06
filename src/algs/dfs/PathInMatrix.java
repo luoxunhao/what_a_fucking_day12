fe0524bccd3d447d48100ecd2890605b89ccb9e4
luoxunhao <1215070400@qq.com>
2017-04-06 22:34:46 +0800

    贪心

--- src/algs/dfs/PathInMatrix.java
+++ src/algs/dfs/PathInMatrix.java
@@ -7,28 +7,26 @@ public class PathInMatrix {
     private static int[] dx = {-1,0,1,0};
     private static int[] dy = {0,1,0,-1};
     public boolean hasPath(char[] matrix, int rows, int cols, char[] str){
+        boolean[] visit = new boolean[rows*cols];
         for (int i = 0; i < rows; i++){
             for (int j = 0; j < cols; j++){
-                if (matrix[i*rows+j] == str[0]){
-                    boolean[] visit = new boolean[rows*cols];
+                //if (matrix[i*rows+j] == str[0]){
+                    //boolean[] visit = new boolean[rows*cols];
                     if (dfs(matrix,rows,cols,str,visit,i,j,0)){
                         return true;
                     }
-                }
+                //}
             }
         }
         return false;
     }
     public static boolean dfs(char[] matrix, int rows, int cols, char[] str, boolean[] visit, int cx, int cy, int pos){
         if (pos == str.length-1){
-            for (int i = 0; i < 4; i++){
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && matrix[nx*cols + ny] == str[pos] && !visit[nx*cols + ny]){
-                    return true;
-                }
+            if (matrix[cx*rows+cy] == str[pos]){
+                return true;
+            }else {
+                return false;
             }
-            return false;
         }
         visit[cx*cols + cy] = true;
         for (int i = 0; i < 4; i++){
@@ -40,6 +38,7 @@ public class PathInMatrix {
                 }
             }
         }
+        visit[cx*cols + cy] = false;
         return false;
     }
 }
