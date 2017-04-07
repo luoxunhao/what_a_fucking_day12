09f4c6aa46e8e6ca789842bd76da71d876e652ef
luoxunhao <1215070400@qq.com>
2017-04-07 22:35:31 +0800

    dp

--- src/algs/dp/LongestPalindromeSubsequence.java
+++ src/algs/dp/LongestPalindromeSubsequence.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * 给定一个字符串s，你可以从中删除一些字符，使得剩下的串是一个回文串。如何删除才能使得回文串最长呢？
  输出需要删除的字符个数。
  */
-public class LongestPalindromeSubsquence {
+public class LongestPalindromeSubsequence {
 
     public static int palindromeLength(String s){
         char[] s1 = s.toCharArray();
