efd460cb3be8f305babeb2aa9c3ac4086da67c73
luoxunhao <1215070400@qq.com>
2017-04-13 15:17:34 +0800

    dfs

--- src/algs/dfs/SumIsK.java
+++ src/algs/dfs/SumIsK.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * 输入两个整数 n 和 m，从数列1，2，3.......n 中随意取几个数,使其和等于 m ,要求将其中所有的可能组合列出来
  * 按字典顺序输出
  */
-public class PermutationOfSumK {
+public class SumIsK {
     private static int n;
     private static int m;
 
